5fcc44ac62d3eab3995e7f08897a691c6308e08a
Ernestas Zaglinskas <virtualybe@gmail.com>
2019-08-02 20:49:06 +0300

    Update rand_all.py

--- rand_all.py
+++ rand_all.py
@@ -4,8 +4,8 @@ while True:
 	def my_array(length=8):
 		numbers  = '0123456789'
 		letters	 = 'abcdefghijklmnoprstuvz'
-		return ''.join(random.choice(numbers)			# numbers
+		#return ''.join(random.choice(numbers)			# numbers
 		#return ''.join(random.choice(letters)			# letters
-		#return ''.join(random.choice(numbers + letters) 	# numbers and letters
+		return ''.join(random.choice(numbers + letters) 	# numbers and letters
 	for i in range(length))
 	print (my_array(8))
